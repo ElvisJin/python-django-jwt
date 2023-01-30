2bfedcbd42b1e40c1297c2f140d8b86ab5401384
PassionMan1130 <123367712+ElvisJin@users.noreply.github.com>
2023-01-30 23:41:35 +0900

    some fixes

--- jwt_api/settings.py
+++ jwt_api/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+# ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 DATABASES = {}
